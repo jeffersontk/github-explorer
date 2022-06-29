a5c14f5be6fc3f0cd964a642f147a2cc3ec6670b
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-06-29 20:24:47 -0300

    primeiro modulo do curso, configurando na mão o react

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import { render } from 'react-dom'
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
+
+/* import { createRoot } from 'react-dom/client';
+import { App } from './App'
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />); */
